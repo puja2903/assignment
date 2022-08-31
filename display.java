bb9f8be7f4e0cf363b434aace9f0e0da738723cb
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-31 22:41:06 +0530

    Add files via upload

---
+++ display.java
@@ -0,0 +1,17 @@
+package Testfile;
+public class display {
+	private static final char[] Message = null;
+
+	void printMessage(){
+	   System.out.println("Hello My Message");
+	}	  
+	//Save the file as “DisplayMessage.java”  .
+
+	class  MyProgram
+	  {
+	    public static void main(String arg[]){
+	      display program=new display();
+	      program.printMessage();
+	   }}}
+
+
